4cf3a9f46735785703e818867abe2f06dc71269f
L-theRing96 <shenhong5796@gmail.com>
2023-08-27 11:47:00 +0800

    added dns record module

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -5,8 +5,22 @@ resource "cloudflare_zone" "this" {
   zone   = var.zone
 }
 
+resource "cloudflare_origin_ca_certificate" "this" {
+  hostnames          = ["*.${var.zone}", var.zone]
+  requested_validity = 0
+}
+
+# Locals block to handle dynamic Cloudflare expression creation
+locals {
+  # If allowed_countries = ["MY", "SG"], 
+  # countries will be ["ip.geoip.country ne \"MY\"", "ip.geoip.country ne \"SG\""]
+  countries = [for c in var.allowed_countries : "ip.geoip.country ne \"${c}\""]
+  # Eg, "ip.geoip.country ne \"MY\" and ip.geoip.country ne \"SG\""
+  geoblock_whitelist  = join(" and ", local.countries)
+}
+
 resource "cloudflare_filter" "geoblock" {
-  expression = "(ip.geoip.country ne \"MY\" and ip.geoip.country ne \"SG\")"
+  expression = local.geoblock_whitelist
   paused     = false
   zone_id    = cloudflare_zone.this.id
 }
@@ -19,3 +33,19 @@ resource "cloudflare_firewall_rule" "geoblock" {
   zone_id     = cloudflare_zone.this.id
 }
 
+module "dns" {
+  source  = "app.terraform.io/ahlooii/dns/cloudflare"
+  version = "2.0.0"
+
+  zone_id = var.zone
+  default_ttl = 300
+
+  map_of_records = {
+    "${var.zone}" = [
+      {
+        name    = "syncthing"
+        proxied = true
+      }
+    ]
+  }
+}
