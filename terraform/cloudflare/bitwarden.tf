ef9563c0d76fa1df2402ed146a688b35dd092561
L-theRing96 <shenhong5796@gmail.com>
2023-09-09 23:38:25 +0800

    remove lfs + adding provisioner to download bw

--- terraform/cloudflare/bitwarden.tf
+++ terraform/cloudflare/bitwarden.tf
@@ -1,8 +1,17 @@
-resource "null_resource" "example" {
+resource "null_resource" "install_bw_cli" {
   provisioner "local-exec" {
-    command = "echo $PATH"
+    command = <<EOL
+      curl -s -L "https://vault.bitwarden.com/download/?app=cli&platform=linux" -o bw-linux-amd64
+      chmod +x bw-linux-amd64
+      sudo mv bw-linux-amd64 /usr/local/bin/bw
+    EOL
+  }
+  
+  triggers = {
+    always_run = "${timestamp()}"
   }
 }
+
 
 data "bitwarden_item_login" "secret" {
     id = "c90a764c-a2a1-4705-bce8-b078009b7e2b"
